d21575fd520cafb77bf0c4711f8088f317f8d33f
TharunreddyKandadi <110463142+TharunreddyKandadi@users.noreply.github.com>
2024-07-22 18:05:30 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+
+  return (
+    <div className="container">
+      <div className="imgDiv">
+   <img className="imagemove"  src="https://images.unsplash.com/photo-1715151696799-88b7d3b9a25c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D"alt="no"/>
+   <img className="imagemove"  src="https://images.unsplash.com/photo-1715151696799-88b7d3b9a25c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D"alt="no"/>
+   <img className="imagemove"  src="https://images.unsplash.com/photo-1715151696799-88b7d3b9a25c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D"alt="no"/>
+   <img className="imagemove"  src="https://images.unsplash.com/photo-1715151696799-88b7d3b9a25c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D"alt="no"/>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
